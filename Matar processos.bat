44ac9be71d35c0edf59cfa52787ef5931d880fdc
carlinhospvh20 <32315281+carlinhospvh20@users.noreply.github.com>
2020-04-07 20:16:41 -0400

    Create Matar processos.bat

---
+++ Matar processos.bat
@@ -0,0 +1,17 @@
+@echo off
+
+rem Significado do comando
+rem - task = tarefa
+rem - list = lista
+rem - kill = matar
+
+rem parametro do comando
+rem /f = forçar
+rem /t = fechar processos filhos
+rem /im = matar o processo
+
+rem "-" traço pode ser utilizado no lugar do "/"
+
+taskkill /f /t /im Discord.exe
+
+exit
