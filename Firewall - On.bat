78e9782d6cd7a907869b077c0bef8fd6d829242b
carlinhospvh20 <32315281+carlinhospvh20@users.noreply.github.com>
2020-04-19 12:03:26 -0400

    Update Firewall - On.bat

--- Firewall - On.bat
+++ Firewall - On.bat
@@ -5,6 +5,9 @@ color 0F
 REM Lagura e Altura
 mode 59, 30
 
+rem allprofiles = todos usuários
+rem set = Atualiza as configurações das definições.
+
 netsh advfirewall set allprofiles state on
 
 pause
