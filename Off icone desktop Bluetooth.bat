68a36c24e6f565c5cd12004fae5c2ee76bf85620
carlinhospvh20 <32315281+carlinhospvh20@users.noreply.github.com>
2020-04-24 20:15:50 -0400

    Update Off icone desktop Bluetooth.bat

--- Off icone desktop Bluetooth.bat
+++ Off icone desktop Bluetooth.bat
@@ -2,4 +2,15 @@
 
 REG ADD "HKCU\Control Panel\Bluetooth" /V "Notification Area Icon" /T REG_DWORD /D 00000000 /F
 taskkill /f /im explorer.exe
+
+:: Significado do comando
+:: - task = tarefa
+:: - list = lista
+:: - kill = matar
+
+:: parametro do comando
+:: /f = forçar
+:: /t = fechar processos filhos
+:: /im = matar o processo
+
 start explorer.exe
