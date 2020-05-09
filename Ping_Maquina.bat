caf40ff50e175cd8ae8e1faac108ff89857f421e
carlinhospvh20 <32315281+carlinhospvh20@users.noreply.github.com>
2020-05-09 19:18:51 -0400

    Update Ping_Maquina.bat

--- Ping_Maquina.bat
+++ Ping_Maquina.bat
@@ -1,4 +1,6 @@
 @echo off
+@chcp 65001
+cls
 
 title Ping 
 
@@ -8,13 +10,24 @@ goto main
 
 echo.
 
-set /p numeroIP=Digite nome Maquina:
+set /p numeroIP=Digite nome Maquina: 
+set /p numeroSol=Número de solicitações de eco: 
 
-ping %numeroIP% 
+
+ping -n %numeroSol% %numeroIP%  > NUL
+:: -n = Número de solicitações de eco a serem enviadas.
+
 
 :: Aguardando 10 Seg Para Fechar o Programa...
 :: choice -c r -t 10 -d r >nul
+:: -t = tempo (segundos)
+::
+::Exemplo #1 (Aguarda 10 segundos)
+::ping -n 10 127.0.0.1
+::
+::Exemplo #2 (Idêntico ao anterior mas com supressão do resultado de saída)
+::ping -n 10 127.0.0.1 > NUL
+
 
 cls
-
 goto main
